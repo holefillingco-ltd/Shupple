1728551903df0313661bcb406679ba6f80dd5b08
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2019-07-28 23:19:15 +0900

    InitializedCommit

---
+++ Shupple/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Shupple
+//
+//  Created by 磯崎裕太 on 2019/07/28.
+//  Copyright © 2019 HoleFillingCo.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Shupple/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Shupple
+//
+//  Created by 磯崎裕太 on 2019/07/28.
+//  Copyright © 2019 HoleFillingCo.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
