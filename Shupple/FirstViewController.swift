8ca8acb0be2d1c7ede400fef125905aa58020523
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2019-07-30 02:19:47 +0900

    [setup] FireBase

--- Shupple/FirstViewController.swift
+++ Shupple/FirstViewController.swift
@@ -9,12 +9,9 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
