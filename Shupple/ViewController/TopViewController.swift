aea4e40003e3e5ff7177294a3ce1a7ce59d03fb1
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2019-08-14 00:52:25 +0900

    [add] ScrollView_Header

--- Shupple/ViewController/TopViewController.swift
+++ Shupple/ViewController/TopViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TopViewController: UIViewController {
+class TopViewController: UIViewController, UIScrollViewDelegate {
 
     var header: UIView!
     var scrollView = UIScrollView()
@@ -19,12 +19,21 @@ class TopViewController: UIViewController {
         // プロフィール画像
         header = UIView(frame: CGRect(x: 10, y: 20, width: view.frame.width - 10*2, height: 200))
         header.backgroundColor = UIColor.init(red: 157/255, green: 204/255, blue: 224/255, alpha: 1)
-        scrollView.delegate = self as? UIScrollViewDelegate
+        scrollView.delegate = self
         scrollView.addSubview(header)
         
         scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height + 100)
         view.addSubview(scrollView)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        header.frame = CGRect(x: 10, y: 20,
+                              width: view.frame.width - 10*2, height: 200)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 }
