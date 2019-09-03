79e9872bbdc11d3073ecf60b2b5e1177d8c7d0a3
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2019-09-04 02:12:36 +0900

    [add] APIClientclass

---
+++ Shupple/Utility/APIClient.swift
@@ -0,0 +1,25 @@
+//
+//  APIClient.swift
+//  Shupple
+//
+//  Created by 磯崎裕太 on 2019/09/04.
+//  Copyright © 2019 HoleFillingCo.,Ltd. All rights reserved.
+//
+
+import Foundation
+
+class APIClient {
+    
+    /**
+     * POST /users
+     * RegistrationVC
+     */
+    func requestRegistration() {
+    }
+    /**
+     * GET /users
+     * TopVC
+     */
+    func requestGetOpponen() {
+    }
+}
