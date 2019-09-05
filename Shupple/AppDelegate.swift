98d76183069a7a9fa362fe447b8014e7d2fd2013
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-09-05 23:28:14 +0900

    tab落ち着かそう。。

--- Shupple/AppDelegate.swift
+++ Shupple/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let userDefault = UserDefaults.standard
+        userDefault.register(defaults: ["UID":"default"])
         
         /**
          * ログインしている場合TopViewControllerへ遷移
