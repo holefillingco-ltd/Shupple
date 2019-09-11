500bccd73641f2db9368d66a1d704537c180a0c2
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-09-11 21:06:14 +0900

    [add] StaticContentsViewController

---
+++ Shupple/ViewController/StaticContentsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StaticContentsViewController.swift
+//  Shupple
+//
+//  Created by 磯崎 裕太 on 2019/09/11.
+//  Copyright © 2019 HoleFillingCo.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class StaticContentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
